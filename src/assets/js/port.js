baebdc126593bf305ab06cde086e391b6c4ec9ab
eugeniayu99 <eugeniayu99@gmail.com>
2026-02-06 17:08:26 +0900

    포트폴리오 완료

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -1,3 +1,24 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
 export function port() {
-    console.log(port)
+    gsap.registerPlugin(ScrollTrigger);
+
+    const sections = gsap.utils.toArray(".port__item");
+    const container = document.querySelector("#port");
+
+    gsap.to(sections, {
+        xPercent: -120 * (sections.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: container,
+            start: "top 56px",
+            end: () => "+=" + container.offsetWidth,
+            pin: true,
+            scrub: 1,
+            markers: false,
+            invalidateOnRefresh: true,
+            anticipatePin: 1,
+        }
+    });
 }
